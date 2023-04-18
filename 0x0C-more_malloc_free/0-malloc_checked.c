712c8924203bf4335a75d8b57da985b24e2e1871
zakiabouguetab1 <bouguetab.zakia@gmail.com>
2023-04-17 18:25:17 -0700

    dog.h

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,39 +1,20 @@
 #include <stdlib.h>
-		
-#include "main.h"
-		
+#include <stdio.h>
 
-		
 /**
-		
- * *malloc_checked - allocates memory using malloc
-		
- * @b: number of bytes to allocate
-		
- *
-		
- * Return: a pointer to the allocated memory
-		
- */
-		
+  * malloc_checked - Allocates memory
+  * @b: the size to allocate
+  *
+  * Return: Nothing.
+  */
 void *malloc_checked(unsigned int b)
-		
 {
-		
-	void *ptr;
-		
+	void *p;
 
-		
-	ptr = malloc(b);
-		
+	p = malloc(b);
 
-		
-	if (ptr == NULL)
-		
+	if (p == NULL)
 		exit(98);
-		
 
-		
-	return (ptr);
-		
+	return (p);
 }
